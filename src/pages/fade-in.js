42c4610726326c80409d80f5fd0f5b28935277bb
Manav <manav@mrmr.io>
2023-12-29 15:28:23 +0530

    Add delays
    Ref: https://github.com/gregberge/loadable-components/issues/39

---
+++ src/pages/fade-in.js
@@ -0,0 +1,50 @@
+import * as React from "react";
+
+// yarn add @loadable/component
+import loadable from "@loadable/component";
+
+const LoadablePage = () => {
+    console.log("Rendering LoadablePage");
+
+    return (
+        <>
+            <Title />
+            <LoadableComponentContainer />
+        </>
+    );
+};
+
+export const Head = () => <title>Using Loadable Component</title>;
+
+export default LoadablePage;
+
+const Title = () => <h1>Using Loadable Component</h1>;
+
+const LoadableComponentContainer = () => {
+    console.log("Rendering LoadableComponentContainer");
+
+    return <ComponentThatUsesWindowWrappedInLoadable fallback={<Loading />} />;
+};
+
+const wait = async (ms) => {
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("done");
+            resolve();
+        }, ms);
+    });
+};
+
+const delayed = async (fn, ms) => {
+    await wait(ms);
+    return fn();
+};
+
+const ComponentThatUsesWindowWrappedInLoadable = loadable(async () => {
+    return delayed(() => import("../components/ComponentThatUsesWindow"), 2000);
+});
+
+const Loading = () => {
+    console.log("Rendering Loading");
+    return <h1>Loading</h1>;
+};
